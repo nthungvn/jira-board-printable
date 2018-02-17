c153805a8791aebc4acc9101bbc8edd5f5201974
Hung Nguyen <nthung.vlvn@gmail.com>
2018-02-17 13:02:08 +0700

    Move configure vue-resource to external file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
-
-Vue.use(VueResource);
-
-Vue.http.options.root = process.env.API_BASE_PATH;
-Vue.http.headers.common['Authorization'] = process.env.API_AUTHORIZATION;
 
 new Vue({
   el: '#app',
