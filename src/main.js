ea19e2355993532dac739a38395042a29a97e573
Hung Nguyen <nthung.vlvn@gmail.com>
2018-02-20 21:30:25 +0700

    Add vuetify css framework

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import App from './App.vue';
+
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
