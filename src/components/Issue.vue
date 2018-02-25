c4cf1d88ef60bad2df414751005c31956cc34fd9
Hung Nguyen <nthung.vlvn@gmail.com>
2018-02-25 22:48:54 +0700

    Add Issue component

---
+++ src/components/Issue.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-layout wrap justify-end class="issue">
+      <v-flex xs11>
+        <v-avatar size="16px">
+          <img :src="value.issueTypeUrl" alt="Issue type">
+        </v-avatar>
+        <v-avatar size="16px">
+          <img :src="value.priorityUrl" alt="Priority">
+        </v-avatar>
+      </v-flex>
+      <v-flex xs1>
+        <v-avatar size="32px">
+          <img :src="value.avatarUrl" alt="Assignee">
+        </v-avatar>
+      </v-flex>
+
+      <v-flex>
+        <v-breadcrumbs divider="/">
+          <v-breadcrumbs-item :href='value.parentIssueKey.href' v-if='value.parentIssueKey.key != ""'>
+            {{ value.parentIssueKey.key }}
+          </v-breadcrumbs-item>
+          <v-breadcrumbs-item :href='value.issueKey.href'>
+            {{ value.issueKey.key }}
+          </v-breadcrumbs-item>
+        </v-breadcrumbs>
+      </v-flex>
+
+      <v-flex class="pt-4" xs12>
+        <div class="title">{{ value.summary }}</div>
+      </v-flex>
+
+      <v-flex class="pt-5" xs1>
+        <v-badge color="grey">
+          <span slot="badge">{{ value.issuePoints }}</span>
+        </v-badge>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data () {
+    return {};
+  }
+}
+</script>
+
+<style scope>
+.issue {
+  border: 1px solid grey;
+  padding: 20px;
+  min-height: 6cm;
+}
+
+.breadcrumbs {
+  padding: 0px;
+}
+
+@media print {
+  .issue {
+    page-break-inside: avoid;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 
 import App from './App.vue';
+import Issue from './components/Issue.vue';
 
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
+
+Vue.component('j-issue', Issue);
 
 new Vue({
   el: '#app',
