4b2c9614944146007ef46fdf04f3efe366ea30fd
Hung Nguyen <nthung.vlvn@gmail.com>
2018-03-19 22:53:57 +0700

    Change the way import vuex and vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import VueRouter from 'vue-router';
-import Vuex from 'vuex';
 
 import App from './App.vue';
 import Issue from './components/Issue.vue';
@@ -11,19 +9,17 @@ import 'vuetify/dist/vuetify.min.css';
 import './assets/css/jira-board.less';
 import './assets/css/jira-board-print.less';
 
-import { routes } from './configs/vue-router';
+import router from './configs/vue-router';
+import store from './store';
 
 Vue.use(Vuetify);
-Vue.use(VueRouter);
-Vue.use(Vuex);
 
 Vue.component('j-issue', Issue);
 Vue.component('j-toolbar', Toolbar);
 
-const router = new VueRouter({ routes });
-
 new Vue({
   el: '#app',
-  router: router,
+  router,
+  store,
   render: h => h(App)
 });
