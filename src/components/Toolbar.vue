49455de61b78638ce9000500cf052183a91c75f3
Hung Nguyen <nthung.vlvn@gmail.com>
2018-03-22 22:26:48 +0700

    Add restrict mode to prevent user change state directly

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -58,7 +58,7 @@ export default {
         return this.$store.state.sprintName;
       },
       set(value) {
-        this.$store.state.sprintName = value;
+        this.$store.commit('sprintName', value);
       }
     }
   },
--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,16 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
     sprintName: process.env.INITIALIZATION_SPRINT_BOARD
   },
 
   getters: {
     sprintName: state => state.sprintName,
+  },
+
+  mutations: {
+    sprintName: (state, value) => state.sprintName = value
   }
 });
