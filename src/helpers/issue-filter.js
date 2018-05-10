6396311e2234631a5354e4607d2b9311afa9e33c
Hung Nguyen <nthung.vlvn@gmail.com>
2018-05-10 23:09:56 +0700

    Can filter task is done out

--- src/helpers/issue-filter.js
+++ src/helpers/issue-filter.js
@@ -1,23 +1,38 @@
 import RegrexFilter from './regrex-filter';
 import Optional from './optional';
+import StringUtils from './string-utils';
 import Store from '../store';
 
 export default class IssueFilter {
-  for(issues) {
+  for (issues) {
     this.issues = issues;
     return this;
   }
 
   filter() {
-    Optional.ofNullable(Store.state.filters.include).ifPresent(pattern => {
-      let filter = new RegrexFilter(pattern);
-      this.issues = this.issues.filter(issue => filter.testInclude(issue.summary))
-    });
-    Optional.ofNullable(Store.state.filters.exclude).ifPresent(pattern => {
-      let filter = new RegrexFilter(pattern);
-      this.issues = this.issues.filter(issue => filter.testExclude(issue.summary))
-    });
+    Optional.ofNullable(Store.state.filters.include)
+      .filter(StringUtils.isNotBlank)
+      .ifPresent(pattern => {
+        let filter = new RegrexFilter(pattern);
+        this.issues = this.issues.filter(issue => filter.testInclude(issue.summary))
+      });
+    Optional.ofNullable(Store.state.filters.exclude)
+      .filter(StringUtils.isNotBlank)
+      .ifPresent(pattern => {
+        let filter = new RegrexFilter(pattern);
+        this.issues = this.issues.filter(issue => filter.testExclude(issue.summary))
+      });
+    Optional.ofNullable(Store.state.filters.isDoneItems)
+      .filter(value => value === false)
+      .ifPresent(value => this.issues = this.issues.filter(this.__isNotDone));
     return this.issues;
+  }
+
+  __isNotDone(issue) {
+    return !Optional.ofNullable(issue)
+      .map(issue => issue.status)
+      .map(status => status === "Resolved" || status === "Closed")
+      .orElse(false);
   }
 }
 
