addd708e588e10752c173dcfbc2fbc96a8028f40
Hung Nguyen <nthung.vlvn@gmail.com>
2018-09-23 21:56:32 +0700

    Add documentation

--- src/helpers/jql-builder.js
+++ src/helpers/jql-builder.js
@@ -28,6 +28,10 @@ export default class JqlBuilder {
     return this;
   }
 
+  /**
+   * Filters by sprint
+   * @param {String} sprint Sprint name or Sprint ID/code
+   */
   sprint(sprint) {
     if (StringUtils.isNotEmpty(sprint)) {
       this.stringBuilder.push(`Sprint = ${sprint}`);
@@ -35,6 +39,10 @@ export default class JqlBuilder {
     return this;
   }
 
+  /**
+   * Filters issue type
+   * @param {String} funtionType Standard function of issue type
+   */
   issueType(funtionType) {
     if (StringUtils.isNotEmpty(funtionType)) {
       this.stringBuilder.push(`issuetype in ${funtionType}`)
@@ -42,16 +50,27 @@ export default class JqlBuilder {
     return this;
   }
 
+  /**
+   * Sorts as ascending
+   * @param {String} by Field will be sorted
+   */
   orderAsc(by) {
     this.stringBuilder.push(`ORDER BY ${by} ASC`);
     return this;
   }
 
+  /**
+   * Sorts as descending
+   * @param {String} by Field will be sorted
+   */
   orderDesc(by) {
     this.stringBuilder.push(`ORDER BY ${by} DESC`);
     return this;
   }
 
+  /**
+   * Returns a JQL query
+   */
   toString() {
     return this.stringBuilder.join(" ");
   }
